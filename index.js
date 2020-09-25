97147cfcbc0964cdd52d0c3224bd582b8125ab7f
TheProgrammer33 <dylan.bryant33@yahoo.com>
2020-09-24 22:10:23 -0400

    Updated set/get messages from the database

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
-var express = require("express");  
-var app = express();  
+var express = require("express");
+var app = express();
+var query = require('url');
 var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/";
 
-app.get("/", function(req, res) {  
-  res.send("Hello World!");  
-});  
-
-app.get("/users", function() {  
-  MongoClient.connect(url, function(err, db) {  
-    if (err) {console.info("An error Occured");}
-    console.log("Database created!");
-    db.close(); 
-  });  
+app.get("/", function(req, res) {
+  res.send("Pong");
 });
 
-app.listen(3000,function(){  
-    console.log('Express app start on port 3000')  
+app.get("/messages", function(req, res) {
+  console.info("Start the thing here!@#$%^&*(): ", query.parse(req.url, true).query);
+  getFromDatabase(req, 'Messages', res)
 });
+
+app.get("/messages/add", function(req, res) {
+  data = query.parse(req.url, true).query
+  if (!data.user || !data.message) {
+    res.send(invalidData());
+  }
+
+  setToDatabase(data, 'Messages', res);
+});
+
+app.listen(3000,function(){
+  console.log('Express app start on port 3000')
+});
+
+function getFromDatabase(data, collection, response) {
+  MongoClient.connect("mongodb://localhost:27017/", function (err, client) {
+    var db = client.db('testDB');
+
+    db.collection(collection).find({}).toArray(function (err, result) {
+            if (err) throw err;
+            response.send(result);
+    });
+    client.close();
+  });
+}
+
+function setToDatabase(data, collection, response) {
+  MongoClient.connect("mongodb://localhost:27017/", function (err, client) {
+    var db = client.db("testDB");
+
+    console.info(data);
+    db.collection(collection).insertOne(data, function (err, result) {
+            if (err) throw err;
+            console.info("Data Saved");
+            response.send(result.ops);
+    });
+    client.close();
+  });
+
+}
+
+function invalidData() {
+  return "400: Bad Request"
+}
